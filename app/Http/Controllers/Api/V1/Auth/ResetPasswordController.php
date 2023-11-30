055792da31041ae0f5dae0b900430a5fb51ecd54
Afrimadoni Dinata <doenikoe@yahoo.com>
2023-11-30 16:23:33 +0700

    fix: missing file

---
+++ app/Http/Controllers/Api/V1/Auth/ResetPasswordController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiBaseController;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+
+class ReseetPasswordController extends ApiBaseController
+{
+
+}
